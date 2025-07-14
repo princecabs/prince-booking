7be3f89671e89b7caab34ed100e13d23d5a0b1a0
princecabs <swmart4@gmail.com>
2025-07-14 19:19:34 +1000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '40px' }}>
+      <img src={logo} alt="Prince Cabs" style={{ width: '150px', marginBottom: '20px' }} />
+      <h2>Book Your Ride with Prince</h2>
+    </div>
+  );
+}
+
+export default App;
